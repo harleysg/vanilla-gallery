83d018144a7eeabb89b1b4f583a6dac521e0f898
harley santos garzon <harley.santos.garzon@gmail.com>
2023-10-11 20:55:52 -0500

    refactor(main): add function to create html

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 import './globals.css'
+
+const app = document.getElementById('app')
+
+function html (tagName, props = {}, children = []) {
+  const newElm = Object.assign(document.createElement(tagName), props)
+
+  if (Array.isArray(children)) {
+    children.forEach(child => {
+      if (child.nodeType && (child.nodeType === 1 || child.nodeType === 11)) {
+        newElm.appendChild(child)
+      } else {
+        newElm.innerHTML += child
+      }
+    })
+  }
+
+  return newElm
+}
+
+const span = (() => {
+  const children = document.createElement('span')
+  children.textContent = 'World'
+  return html('p', {
+    className: 'awesome-paragraph',
+    innerHTML: '<strong>Hello</strong> '
+  }, [children])
+})()
+
+app.appendChild(span)
