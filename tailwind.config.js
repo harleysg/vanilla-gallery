a403c6ecf69c844a6c32a163fe7bdd00b772daec
harley santos garzon <harley.santos.garzon@gmail.com>
2023-10-13 16:28:52 -0500

    refactor(tailwind): improve config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   content: [
     './index.html',
     './*.{js}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      boxShadow: {
+        highlight: 'inset 0 0 0 1px rgba(255, 255, 255, 0.05)'
+      },
+      screens: {
+        narrow: { raw: '(max-aspect-ratio: 3 / 2)' },
+        wide: { raw: '(min-aspect-ratio: 3 / 2)' },
+        'taller-than-854': { raw: '(min-height: 854px)' }
+      }
+    }
   },
   plugins: []
 }
