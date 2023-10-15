e4c27b1392f85c83b705a522506c8e437f3ab8f9
harley santos garzon <harley.santos.garzon@gmail.com>
2023-10-15 05:51:08 -0500

    refactor(gallery): improve  catch error

--- gallery.mjs
+++ gallery.mjs
@@ -2,10 +2,14 @@ import { modalCarousel } from './services/modal-carousel.js'
 import { galleryService } from './services/gallery.service.js'
 
 export async function vanillaGallery ({ gallery, dialog }) {
-  const modalCarouselRef = modalCarousel({ dialog })
+  const modalCarouselRef = await modalCarousel({ dialog }).catch(async error => {
+    const val = await error
+    return val?.callback
+  })
+
   modalCarouselRef.initCarousel()
 
-  galleryService({
+  await galleryService({
     gallery,
     option: {
       emitImageSelected: modalCarouselRef.getImagesFromGallery
