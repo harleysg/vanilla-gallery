f69c74d4af8aa82b6af004ae5674833f882d4308
harley santos garzon <harley.santos.garzon@gmail.com>
2023-10-15 05:39:29 -0500

    refactor(renderImage): include async method
    getImage method to query images

--- services/_renderImages.js
+++ services/_renderImages.js
@@ -1,24 +1,52 @@
 import { IMAGES } from '../db/images.js'
+import { HTMLError } from '../helpers/errors.js'
+import { getImages } from '../utils/index.js'
 import html from './createHtml.js'
 
-export async function _renderImages (target, options = {}) {
-  if (!target) return
+const defaultOptions = {
+  props: {},
+  getOuterHTML: false,
+  error: { origin: '' }
+}
 
-  IMAGES.forEach((image, index) => {
+export async function _renderImages (
+  target, { props, getOuterHTML, error } = defaultOptions
+) {
+  if (!target || !document.querySelector(target?.tagName)) {
+    throw new HTMLError('The target element is not defined', {
+      origin: `renderImages <- ${error?.origin ?? ''}`
+    })
+  }
+
+  target.toggleAttribute('data-loading')
+  const data = await getImages(IMAGES)
+  let tmplImage = ''
+
+  if (data.length === 0) return
+
+  data.forEach((image, index) => {
     if (image.src) {
       const img = html('a', {
         innerHTML: `<img
           src="${image.src}"
           data-position="${index}"
-          class="object-cover h-full w-full brightness-90 group-hover:brightness-110 "
-        />`,
+          loading="lazy"
+          class="object-cover h-full w-full brightness-90 group-hover:brightness-110 object-center aspect-[3/2]"
+          />`,
         href: `#${index}`,
-        ...options
+        ...props
       })
 
       img.dataset.position = index
 
-      target.appendChild(img)
+      if (getOuterHTML) {
+        tmplImage += img.outerHTML
+      } else {
+        target.appendChild(img)
+      }
     }
   })
+
+  getOuterHTML && (target.innerHTML = tmplImage)
+  target.toggleAttribute('data-loading')
 }
