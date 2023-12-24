2d047ef13f230d8003a104a474a2245321cc3917
harley santos garzon <harley.santos.garzon@gmail.com>
2023-12-23 21:15:54 -0500

    style(utils) adjust type validator

--- utils/general/index.js
+++ utils/general/index.js
@@ -1,34 +1,35 @@
-import { loadImage } from '../dom'
-
-function confirmTypeOf (value) {
-  return {
+export function confirmTypeOf (value) {
+  const validations = {
     isArray: Array.isArray(value),
     isString: typeof value === 'string',
     isObject: typeof value === 'object',
     isObjectStringified: _isObjectStringified(value),
     isNull: Object.is(value, null),
     isUndefined: Object.is(value, undefined),
-    isNotValid: !value || Object.is(value, null) || Object.is(value, undefined)
+    isNumber: typeof value === 'number',
+    isBigInt: _isBigInt(value)
+  }
+  const result = Object.keys(validations).map(k => validations[k])
+
+  return {
+    ...validations,
+    result
   }
 }
 
-function _isObjectStringified (params) {
-  if (typeof params === 'string' && params.length > 0) {
-    return !!params.charAt(0).match(/[{|/\\[/]/g) && !!params.charAt(params.length - 1).match(/[}|/\]/]/g)
+function _isBigInt (value) {
+  return !Object.is(value, null) &&
+    !Object.is(value, undefined) &&
+    typeof value !== 'object' &&
+    typeof value !== 'string' &&
+    typeof value !== 'number' &&
+    BigInt(value) === value
+}
+
+function _isObjectStringified (value) {
+  if (typeof value === 'string' && value.length > 0) {
+    return !!value.charAt(0).match(/[{|/\\[/]/g) && !!value.charAt(value.length - 1).match(/[}|/\]/]/g)
   }
+
   return false
-}
-
-async function getImages (listOfImages = []) {
-  // TODO: Save on localStorage to avoid duplicated calls | loads
-  const _Proimses = await Promise.allSettled(listOfImages.map(row => loadImage(row.src || '')))
-
-  return _Proimses
-    .filter(x => x.status === 'fulfilled')
-    .map(x => x.value)
-}
-
-export {
-  confirmTypeOf,
-  getImages
 }
